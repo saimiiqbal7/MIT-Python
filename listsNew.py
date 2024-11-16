dfe57e227f86f9b98ab2533c962162548069e0b0
saimiiqbal7 <saimiiqbal7@gmail.com>
2024-11-16 20:29:27 -0330

    fun stuff

---
+++ listsNew.py
@@ -0,0 +1,9 @@
+
+
+'''
+del(l[index]) removes something at given index
+l.pop() -> returns last element
+l.pop(index) -> returns element at index
+l.remove() - > removes the first instance of given element 
+
+'''

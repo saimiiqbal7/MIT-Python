e1cb0487429feeaa1a20709897c07c8845b4bf55
saimiiqbal7 <saimiiqbal7@gmail.com>
2024-11-16 20:31:40 -0330

    funf

--- listsNew.py
+++ listsNew.py
@@ -7,3 +7,9 @@ l.pop(index) -> returns element at index
 l.remove() - > removes the first instance of given element 
 
 '''
+
+l = [2,1,3,6,3,2,7,9]
+print(l)
+elem = l.pop(1)
+print(l)
+print(elem)
